94338e859a1af986f33fd6a05b9ec30e20fda2e0
mavjester <mavjester_github@users.noreply.github.com>
2024-01-21 11:05:06 +0000

    Changes polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -3,13 +3,9 @@ from django.http import HttpResponse
 from .models import Question
 
 def index(request):
-<<<<<<< HEAD
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     output = ", ".join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
-=======
-    return HttpResponse("Hello, world. You're at the polls index.")
->>>>>>> features/be/view
 
 
 def detail(request, question_id):
@@ -17,18 +13,9 @@ def detail(request, question_id):
 
 
 def result(request, question_id):
-<<<<<<< HEAD
     response = "You're looking at the results of question %s."
-=======
-    response = "You're looking at the result of question %s."
->>>>>>> features/be/view
     return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
-<<<<<<< HEAD
-=======
-#
->>>>>>> features/be/view
